9a14950153af21966b142288e5f68f1cb15649a6
Karavarichyan <narekkaravarichyan1910@mail.ru>
2023-01-07 18:24:12 +0400

    img fix

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -5,7 +5,7 @@
         <h1 class="title-1">Video service</h1>
 
         <img
-          src="./img/projects/02-big.jpg"
+          src="@/img/projects/02-big.jpg"
           alt=""
           class="project-details__cover"
         />
@@ -15,7 +15,7 @@
         </div>
 
         <a href="#!" class="btn-outline">
-          <img src="./img/icons/gitHub-black.svg" alt="" />
+          <img src="@/img/icons/gitHub-black.svg" alt="" />
           GitHub repo
         </a>
       </div>
@@ -27,20 +27,20 @@
       <div class="footer__wrapper">
         <ul class="social">
           <li class="social__item">
-            <a href="#!"><img src="./img/icons/vk.svg" alt="Link" /></a>
+            <a href="#!"><img src="@/img/icons/vk.svg" alt="Link" /></a>
           </li>
           <li class="social__item">
-            <a href="#!"><img src="./img/icons/instagram.svg" alt="Link" /></a>
+            <a href="#!"><img src="@/img/icons/instagram.svg" alt="Link" /></a>
           </li>
           <li class="social__item">
-            <a href="#!"><img src="./img/icons/twitter.svg" alt="Link" /></a>
+            <a href="#!"><img src="@/img/icons/twitter.svg" alt="Link" /></a>
           </li>
           <li class="social__item">
-            <a href="#!"><img src="./img/icons/gitHub.svg" alt="Link" /></a>
+            <a href="#!"><img src="@/img/icons/gitHub.svg" alt="Link" /></a>
           </li>
           <li class="social__item">
             <a href="https://www.linkedin.com/in/narek-karavarichyan-5b8573222/"
-              ><img src="./img/icons/linkedIn.svg" alt="Link"
+              ><img src="@/img/icons/linkedIn.svg" alt="Link"
             /></a>
           </li>
         </ul>
